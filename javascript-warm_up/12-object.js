df4e65b096a3260732bec3b8c066403fe2a46ad7
Lesego <llputsoa@gmail.com>
2024-01-31 20:50:56 +0200

    myObject

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+//Script that replace the value in an object
+
+const myObject = {
+    type: 'object',
+    value: 12
+};
+    console.log(myObject)
+
+myObject.value = 89
+    console.log(myObject)
