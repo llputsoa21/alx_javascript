2320239d1e51dc5d076bdddfb075e9e89ea7c076
Lesego <llputsoa@gmail.com>
2024-01-31 20:24:46 +0200

    Print 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+//Script that prints 3 lines
+
+let firstchar = "C is fun";
+let secondchar = " Python is cool";
+let thirdchar = "JavaScript is amazing";
+
+console.log(firstchar);
+console.log(secondchar);
+console.log(thirdchar);
