237aea6806a77d83389d6d5d6989efc36cc6f355
Lesego <llputsoa@gmail.com>
2024-01-31 22:05:28 +0200

    Class rectangle

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+//Class rectangle that is initialized by the following constructors
+
+class Rectangle{
+    constructor(w, h){
+        if (w>0 && h>0){
+            this.width = w;
+            this.height = h;
+
+        };
+
+        };
+};
+module.exports = Rectangle;
