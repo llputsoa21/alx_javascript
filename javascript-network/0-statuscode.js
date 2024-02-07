994a523a927f0cedd0de3440c7b68550f423a8a1
Lesego <llputsoa@gmail.com>
2024-02-07 07:41:55 +0200

    JS partial Commit

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,2 +1,11 @@
 // Arguments and status codes
 
+const process = require('process')
+const request = require('request')
+const requestURL = process.argv[2]
+
+request(requestURL, function(error, response){
+    if(error == null){
+        console.log('code: ' + response.statusCode);
+    };
+});
--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,1 +1,14 @@
 // Star Wars API 
+
+const process = require('process')
+const request = require('request')
+const endpoint = 'https://swapi-api.alx-tools.com/api/films'
+const episode = process.argv[2]
+
+request(`${endpoint}/${episode}`, function(error, response, body){
+    if(error){
+        console.log(error)
+    }
+    const result = JSON.parse(body)
+    console.log(result['title'])
+});
